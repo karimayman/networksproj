dd8aae42e9fc17892bce62dc5e8ebf7a4b8dc22d
karimayman <karimayman95@gmail.com>
2019-04-23 20:32:18 +0200

    project finalized and cleaned

--- project2.py
+++ project2.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, render_template,g, request, redirect, url_for, \
-    jsonify
+    jsonify 
 from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Questions, Choices, Used
@@ -8,32 +8,52 @@ from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 from flask import session as var_session
 import random
 import string
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
 import httplib2
 import json
 from flask import make_response
 import requests
 app = Flask(__name__)
 
+ 
 engine = create_engine('sqlite:///test.db')
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
 #session = DBSession()
-@app.route('/nothere')
-def notsafe():
+@app.route('/failsafe')
+def danger():
+
     session = DBSession()
     used = session.query(Used).all()
     for i in used:
         session.delete(i)
         session.commit() 
     session.close()
+    used = session.query(Used).all()
+    var_session.clear()
+    return render_template('failsafe.html')
+ 
+
+@app.route('/nothere')
+def notsafe():
+
+    session = DBSession()
+    used = session.query(Used).all()
+    for i in used:
+        session.delete(i)
+        session.commit() 
+    session.close()
+    used = session.query(Used).all()
+    var_session.clear()
     return redirect(url_for('questionmaker'))
 
 @app.route('/')
 def questionmaker():
-    
+    if var_session.get('used_question')==None:
+        picked=randomizer()
+
+        var_session['used_question']=picked
+    else:
+        picked = var_session['used_question']    
     session = DBSession()
     # if var_session.get('used_question')!=None:
     #     if (len(g.used_question) < 1 ) or (len(g.used_question) < 2 ):   
@@ -41,17 +61,16 @@ def questionmaker():
     #     else:
     #         picked = g.used_question[1:]
     # else:
-    picked=randomizer()
+
+    questions = session.query(Questions).all()  
+    choices = session.query(Choices).all()
+    session.close()        
     if 0 not in picked:
-        questions = session.query(Questions).all()  
-        choices = session.query(Choices).all()
-        
-        #     picked = randomizer()
-        print(picked)
-        session.close()
-        return render_template('questiontodelete.html' ,choices = choices,questions=questions,picked=picked)
+
+        return render_template('question.html' ,choices = choices,questions=questions,picked=picked)
     else:
         return render_template('full.html')
+   
 @app.route('/graded', methods=['POST'])
 def grader():
     grade = 0
@@ -62,7 +81,6 @@ def grader():
             questions = session.query(Questions).filter_by(id=i).one()   
             if request.form[i] == questions.answer:
                 count+=1   
-            print(questions.id)
             usedquest = session.query(Used).filter_by(question_id=i).one()
             session.delete(usedquest)
             session.commit()    
@@ -74,6 +92,8 @@ def grader():
         
         session.close()
       
+        del var_session['used_question']
+        var_session.clear()
 
     return render_template('grade.html',grade = count, count =correct)
 def randomizer():
@@ -83,20 +103,18 @@ def randomizer():
     try:
         if  session.query(Used).one_or_none() != None:
             used=session.query(Used).all()
+            used_question=[]
             for i in used: 
-                used_question=used.question_id
+                used_question.append(i.question_id)
         else:
             used_question=[]
     except MultipleResultsFound:
         used=session.query(Used).all()
-        print(used)
         used_question=[]
         for i in used: 
             used_question.append(i.question_id)
     tbu=[]
-    
-    print(used_question)
-    if len(used_question) < last.id-3:
+    if len(used_question) < last.id-1:
         for i in range(3):
             not_used=random.choice([x for x in range(1,last.id+1) if x not in used_question])
             newrec = Used(question_id=not_used)
@@ -107,10 +125,20 @@ def randomizer():
         session.close()
     else:
         tbu=[0]
-    print(used_question)
-    print(tbu)
     return tbu 
 
+# @app.route('/unfinished', methods=['POST'])
+# def unfinished():
+#     session = DBSession()
+#     for i in var_session['used_question']:  
+#         print(var_session['used_question'])
+#         usedquest = session.query(Used).filter_by(question_id=i).one()
+#         session.delete(usedquest)
+#         session.commit()    
+#     var_session.clear()
+#     session.close()
+#     print("hello hello hello i worked and everything is cool")
+#     return
 
 
 
--- questions.py
+++ questions.py
@@ -4,142 +4,440 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Questions, Base, Choices
 
 engine = create_engine('sqlite:///test.db')
-# Bind the engine to the metadata of the Base class so that the
-# declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-# A DBSession() instance establishes all conversations with the database
-# and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
+
 session = DBSession()
 
 
-# Menu for UrbanBurger
-q = Questions(title="dummy 1?",answer="karim")
+
+q = Questions(title="Which cut flower is the most popular for purchase?",answer="Rose")
 
 session.add(q)
 session.commit()
 
-q = Questions(title="dummy 2?",answer="voila this is karim")
+q = Questions(title="Which flower has the smallest seeds?",answer="Orchid")
 
 session.add(q)
 session.commit()
-q = Questions(title="dummy 3?",answer="karim again")
-
-session.add(q)
-session.commit()
-
-
-
-c= Choices(choice = "yes",question_id="1")
-
-session.add(c)
-session.commit()
-
-c= Choices(choice = "maybe",question_id="1")
-
-session.add(c)
-session.commit()
-
-c= Choices(choice = "no",question_id="1")
-
-session.add(c)
-session.commit()
-
-c= Choices(choice = "yes",question_id="2")
-
-session.add(c)
-session.commit()
-
-c= Choices(choice = "maybe",question_id="2")
-
-session.add(c)
-session.commit()
-
-c= Choices(choice = "no",question_id="2")
-
-session.add(c)
-session.commit()
-
-c= Choices(choice = "yes",question_id="3")
-
-session.add(c)
-session.commit()
-
-c= Choices(choice = "maybe",question_id="3")
-
-session.add(c)
-session.commit()
-
-c= Choices(choice = "no",question_id="3")
-
-session.add(c)
-session.commit()
-
-q = Questions(title="dummy 4?",answer="karim")
-
-session.add(q)
-session.commit()
-
-q = Questions(title="dummy 5?",answer="voila this is karim")
-
-session.add(q)
-session.commit()
-q = Questions(title="dummy 6?",answer="karim again")
+q = Questions(title="Which flower symbolically means Beauty?",answer="Calla Lily")
 
 session.add(q)
 session.commit()
 
 
 
-c= Choices(choice = "yes",question_id="4")
+c= Choices(choice = "Rose",question_id="1")
 
 session.add(c)
 session.commit()
 
-c= Choices(choice = "maybe",question_id="4")
+c= Choices(choice = "Lily",question_id="1")
 
 session.add(c)
 session.commit()
 
-c= Choices(choice = "no",question_id="4")
+c= Choices(choice = "Tulip",question_id="1")
 
 session.add(c)
 session.commit()
 
-c= Choices(choice = "yes",question_id="5")
+c= Choices(choice = "Daffodil",question_id="2")
 
 session.add(c)
 session.commit()
 
-c= Choices(choice = "maybe",question_id="5")
+c= Choices(choice = "Orchid",question_id="2")
 
 session.add(c)
 session.commit()
 
-c= Choices(choice = "no",question_id="5")
+c= Choices(choice = "Sweet Pea",question_id="2")
 
 session.add(c)
 session.commit()
 
-c= Choices(choice = "yes",question_id="6")
+c= Choices(choice = "Honeysuckle",question_id="3")
 
 session.add(c)
 session.commit()
 
-c= Choices(choice = "maybe",question_id="6")
+c= Choices(choice = "Gardenia",question_id="3")
 
 session.add(c)
 session.commit()
 
-c= Choices(choice = "no",question_id="6")
+c= Choices(choice = "Calla Lily",question_id="3")
 
 session.add(c)
 session.commit()
 
-print "added menu items!"
+q = Questions(title="What color rose would you send to someone to say: i am innocent and pure?",answer="White")
+
+session.add(q)
+session.commit()
+
+q = Questions(title="The disc of this flower is made up of hundreds of tiny flowers:",answer="Daisy")
+
+session.add(q)
+session.commit()
+q = Questions(title="What part of the flower is the corolla",answer="Petals")
+
+session.add(q)
+session.commit()
+ 
+
+c= Choices(choice = "Pink",question_id="4")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "White",question_id="4")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Yellow",question_id="4")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Anemone",question_id="5")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Gardenia",question_id="5")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Daisy",question_id="5")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Stamen",question_id="6")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Petals",question_id="6")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Pistil",question_id="6")
+
+session.add(c)
+session.commit()
+
+
+q = Questions(title="What flower bulb can be used in place of onions in cooking?",answer="Tulip")
+
+session.add(q)
+session.commit()
+
+q = Questions(title="Who said : where flowers bloom so does hope?",answer="Lady Bird Johnson")
+
+session.add(q)
+session.commit()
+q = Questions(title="If a bride chooses pink roses for her wedding bouquet, what message is she conveying?",answer="Grace and Joy")
+
+session.add(q)
+session.commit()
+
+
+
+c= Choices(choice = "Freesia",question_id="7")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Tulip",question_id="7")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Dahlia",question_id="7")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Lady Bird Johnson",question_id="8")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Shakespeare",question_id="8")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Mother Teresa",question_id="8")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Innocence and Purity",question_id="9")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Modesty",question_id="9")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Grace and Joy",question_id="9")
+
+session.add(c)
+session.commit()
+
+
+q = Questions(title="Which is the biggest flower of the world?",answer="Rafflasia")
+
+session.add(q)
+session.commit()
+
+q = Questions(title="There is a pretty little flower that shares its name with a musical instrument. Which is it?",answer="viola")
+
+session.add(q)
+session.commit()
+q = Questions(title="Which of the following flowers changes colour from blue to pink according to the amount of acid in the soil?",answer="Hydrangea")
+
+session.add(q)
+session.commit()
+
+
+
+c= Choices(choice = "Petunia",question_id="10")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Lily",question_id="10")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Rafflasia",question_id="10")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Drumflower",question_id="11")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Harp flower",question_id="11")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Viola",question_id="11")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Hydrangea",question_id="12")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Daisies",question_id="12")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Marigolds",question_id="12")
+
+session.add(c)
+session.commit()
+
+q = Questions(title="There is a flower with a charming name that makes you think of Christmas. However, it doesn't bloom in December. What is it?",answer="Star Of Bethlehem")
+
+session.add(q)
+session.commit()
+
+q = Questions(title="Which of the following flowers once caused a huge panic and made many people bankrupt?",answer="Tulip")
+
+session.add(q)
+session.commit()
+q = Questions(title="Which flowers are the most popular spring flowers of all time?",answer="Tulips")
+
+session.add(q)
+session.commit()
+
+
+
+c= Choices(choice = "Carolianthus",question_id="13")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Star Of Bethlehem",question_id="13")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Christmas Bush",question_id="13")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Carnation",question_id="14")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Tulip",question_id="14")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Marygold",question_id="14")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Tulips",question_id="15")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Roses",question_id="15")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "SunFlowers",question_id="15")
+
+session.add(c)
+session.commit()
+
+q = Questions(title="What is the genus name of sunflower?",answer="Helianthus")
+
+session.add(q)
+session.commit()
+
+q = Questions(title="Which flowers are gifted to friends on Friendship Day?",answer="White Roses")
+
+session.add(q)
+session.commit()
+q = Questions(title="Which of these flowers is the favorite of over 80 percent of people?",answer="Roses")
+
+session.add(q)
+session.commit()
+ 
+
+c= Choices(choice = "Dianthus",question_id="13")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Helianthus",question_id="13")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Nelumbo",question_id="13")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Yellow Roses",question_id="14")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Pink Roses",question_id="14")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "White Roses",question_id="14")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "SunFlowers",question_id="15")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Roses",question_id="15")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Tulips",question_id="15")
+
+session.add(c)
+session.commit()
+
+q = Questions(title="Saffron, the spice, comes from a type of which flower?",answer="Crocus")
+
+session.add(q)
+session.commit()
+
+q = Questions(title="Which of these flowers does not need soil to grow?",answer="Orchids")
+
+session.add(q)
+session.commit()
+q = Questions(title="Which flower was poisoned by the Wicked Witch in the Wizard of Oz?",answer="Poppy")
+
+session.add(q)
+session.commit()
+
+
+
+c= Choices(choice = "Crocus",question_id="16")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Magnolia",question_id="16")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Iris",question_id="16")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Lilies",question_id="17")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Orchids",question_id="17")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Lavender",question_id="17")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Iris",question_id="18")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Rose",question_id="18")
+
+session.add(c)
+session.commit()
+
+c= Choices(choice = "Poppy",question_id="18")
+
+session.add(c)
+session.commit()
+
+
+print "questions added successfully, ready, set , solve !"
